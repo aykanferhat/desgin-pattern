4f8eccc9b3d450e6f4f05a3207056a8b8160d72a
Ferhat Aykan <ferhat.aykan@sahabt.com>
2018-10-14 20:53:21 +0300

    added factory design pattern

---
+++ creational-design-pattern/factory-design-pattern/src/superClass/PC.java
@@ -0,0 +1,31 @@
+package superClass;
+
+public class PC extends Computer {
+
+    private String ram;
+
+    private String hdd;
+
+    private String cpu;
+
+    public PC(String ram, String hdd, String cpu){
+        this.ram=ram;
+        this.hdd=hdd;
+        this.cpu=cpu;
+    }
+
+    @Override
+    public String getRam() {
+        return ram;
+    }
+
+    @Override
+    public String getHdd() {
+        return hdd;
+    }
+
+    @Override
+    public String getCpu() {
+        return cpu;
+    }
+}
---
+++ creational-design-pattern/factory-design-pattern/src/superClass/Server.java
@@ -0,0 +1,31 @@
+package superClass;
+
+public class Server extends Computer {
+
+    private String ram;
+
+    private String hdd;
+
+    private String cpu;
+
+    public Server(String ram, String hdd, String cpu) {
+        this.ram = ram;
+        this.hdd = hdd;
+        this.cpu = cpu;
+    }
+
+    @Override
+    public String getRam() {
+        return ram;
+    }
+
+    @Override
+    public String getHdd() {
+        return hdd;
+    }
+
+    @Override
+    public String getCpu() {
+        return cpu;
+    }
+}
